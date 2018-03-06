16d639307df81694036ffe536b73e308c35b376c
justasdaugintis <justasd101@gmail.com>
2018-03-06 10:36:14 +0000

    bit of change

--- javascript.js
+++ javascript.js
@@ -4,11 +4,18 @@ function newElement() {
     var inputValue = document.getElementById("myInput").value;
     var t = document.createTextNode(inputValue);
     li.appendChild(t);
+    li.setAttribute('class', 'listItem') 
 
     if (inputValue === '') {
-      alert("You must write something!");
+      return null
     }
      else {
       document.getElementById("tasks").appendChild(li);  
     }
+
+
+    document.querySelector('.listItem').addEventListener('click', function(){
+
+document.querySelector('.listItem').style.display = "none"
+    })
 }
