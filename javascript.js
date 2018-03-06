2f0a1d99738f7ef39baec25f9be75016c8317573
Stas <scernisevs@gmail.com>
2018-03-06 10:34:33 +0000

    Added a div
    div div divdivdvi

--- javascript.js
+++ javascript.js
@@ -12,3 +12,14 @@ function newElement() {
       document.getElementById("tasks").appendChild(li);  
     }
 }
+
+
+function newDiv() {
+    var li = document.createElement("div");
+    var inputValue = document.getElementById("myInput").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    document.getElementById("bigDiv").appendChild(li);
+}
+
+
